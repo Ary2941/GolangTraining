1d86f11c50f92223f51ef11f133b59b5184cb020
amaury <amaury2941@gmail.com>
2025-01-05 21:40:58 -0300

    arrays!

---
+++ states.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt")
+
+func main () {
+	var listStates = [27]string{}
+	listStates[0] = "Acre";
+	listStates[1] = "Alagoas";
+	listStates[3] = "Amapá";
+	fmt.Println(listStates);
+}
