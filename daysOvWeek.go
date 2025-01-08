1b2bd8c75d6e7d99cf245023317c5c42b4b76d24
amaury <amaury2941@gmail.com>
2025-01-07 22:25:42 -0300

    days of the week

---
+++ daysOvWeek.go
@@ -0,0 +1,23 @@
+package main
+import("fmt")
+
+func main(){
+  day := 3
+  switch day {
+  	case 1:
+		fmt.Println("Domingo")
+	case 2:
+		fmt.Println("Segunda")
+	case 3:
+		fmt.Println("Terça")
+	case 4:
+		fmt.Println("Quarta")
+	case 5:
+		fmt.Println("Quinta")
+	case 6:
+		fmt.Println("Sexta")
+	case 7:
+		fmt.Println("Sábado")
+  }
+
+}
